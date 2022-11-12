4942e952e29fdd6afa7bf6f9c5c82b0424c7dcc1
Sakib Hasan <sakib100.sa@gmail.com>
2022-11-13 05:23:54 +0600

    start writing client side code

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,3 +1,22 @@
 import mongoose from "mongoose";
 // Creating a schema for the database
-const postSchema = mongoose.Schema({});
+const postSchema = mongoose.Schema({
+	title: String,
+	message: String,
+	creator: String,
+	tags: [String],
+	selectedFile: String,
+	likeCount: {
+		type: Number,
+		default: 0,
+	},
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
+});
+
+// Creating a model for the database
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 // Creating posts route for the server
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
