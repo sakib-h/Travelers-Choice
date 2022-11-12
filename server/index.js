8e97b0581b2002742d674fc23cc727fc2b3e1835
Sakib Hasan <sakib100.sa@gmail.com>
2022-11-12 11:48:11 +0600

    server connected to mongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 // initialize express
 const app = express();
+//Using body-parser for app
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// Using cors for app
+app.use(cors());
+// Using dotenv for app
+dotenv.config();
+// Initialize port
+const PORT = process.env.PORT || 5000;
+
+// Connection to Mongoose
+const URL = process.env.CONNECTION_URL;
+mongoose
+	.connect(URL)
+	.then(() => {
+		// Listening to the server
+		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+	})
+	.catch((err) => console.log(err.message));
