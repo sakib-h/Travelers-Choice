0843ff37da44b50c33f7933d9a34cf2dd5b8747c
Sakib Hasan <sakib100.sa@gmail.com>
2022-11-14 04:26:42 +0600

    working on backend crud operations

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,8 +1,17 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 const router = express.Router();
 
 // CREATE
-
+router.post("/", async (req, res) => {
+	const newHotel = new Hotel(req.body);
+	try {
+		const savedHotel = await newHotel.save();
+		res.status(200).json(savedHotel);
+	} catch (error) {
+		res.send(500).json({ error: error.message });
+	}
+});
 // UPDATE
 // DELETE
 // FETCH ONE
