696b2c4a10ee00855c9abf0b4e2f935ca6191b22
Sakib Hasan <sakib100.sa@gmail.com>
2022-11-14 11:31:28 +0600

    crud operation implememt complete on hotel route

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -13,7 +13,35 @@ router.post("/", async (req, res) => {
 	}
 });
 // UPDATE
+router.put("/:id", async (req, res) => {
+	try {
+		const updateHotel = await Hotel.findOneAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.status(200).json(updateHotel);
+	} catch (error) {}
+});
 // DELETE
+router.delete("/:id", async (req, res) => {
+	try {
+		await Hotel.findByIdAndDelete(req.params.id);
+		res.status(200).json("Hotel has been deleted...");
+	} catch (error) {}
+});
 // FETCH ONE
+router.get("/:id", async (req, res) => {
+	const hotel = await Hotel.findById(req.params.id);
+	res.status(200).json(hotel);
+});
 // FETCH ALL
+router.get("/", async (req, res) => {
+	try {
+		const hotels = await Hotel.find();
+		res.status(200).json(hotels);
+	} catch (error) {}
+});
 export default router;
