cfa73f915159292d5a3bc6105431e94d68e6fb87
Sakib Hasan <sakib100.sa@gmail.com>
2022-11-25 19:30:10 +0600

    working on footer section

--- client/src/Components/MailList/MailList.jsx
+++ client/src/Components/MailList/MailList.jsx
@@ -2,23 +2,26 @@ import React from "react";
 
 const MailList = () => {
 	return (
-		<div className="w-full my-[50px] bg-[#003580] text-white p-[50px]">
-			<div className="container flex flex-col items-center gap-[20px] ">
-				<h1 className="font-Source-Sans-Pro text-[24px] font-[700] ">
-					Save Time, Save Money!
-				</h1>
-				<span className="mailDesc">
-					Sign up for our newsletter and get the latest deals to you
-				</span>
-				<div className="flex items-center ">
-					<input
-						type="email"
-						name="email"
-						id="email"
-						placeholder="Enter your Email address"
-						className="mailInput"
-					/>
-					<button className="subscribeButton">Subscribe</button>
+		<div className="w-full my-[50px] bg-[#003580] text-white p-[50px] ">
+			<div className="container">
+				<div className=" flex flex-col items-center gap-[10px] ">
+					<h1 className="font-Source-Sans-Pro text-[24px] font-[700] mb-1">
+						Save Time, Save Money!
+					</h1>
+					<span className="text-[16px] font-[400] mb-2 text-center ">
+						Sign up for our newsletter and get the latest deals to
+						you
+					</span>
+					<div className="flex items-center ">
+						<input
+							type="email"
+							name="email"
+							id="email"
+							placeholder="Enter your Email address"
+							className="mailInput"
+						/>
+						<button className="subscribeButton">Subscribe</button>
+					</div>
 				</div>
 			</div>
 		</div>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Featured from "../../Components/Featured/Featured";
 import FeaturedProperties from "../../Components/FeaturedProperties/FeaturedProperties";
+import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import MailList from "../../Components/MailList/MailList";
 import Navbar from "../../Components/Navbar/Navbar";
@@ -21,6 +22,7 @@ const Home = () => {
 				</h1>
 				<FeaturedProperties />
 				<MailList />
+				<Footer />
 			</div>
 		</section>
 	);
