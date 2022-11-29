b48f39cadf85678bb25378e48513900012882485
Sakib Hasan <sakib100.sa@gmail.com>
2022-11-29 23:32:13 +0600

    adding items

--- client/src/Pages/List/List.jsx
+++ client/src/Pages/List/List.jsx
@@ -86,6 +86,9 @@ const List = () => {
 						<div className="listResult  col-span-3">
 							<h1>Result</h1>
 						</div>
+						<div className="listResult  col-span-3">
+							<h1>Result</h1>
+						</div>
 					</div>
 				</div>
 			</div>
