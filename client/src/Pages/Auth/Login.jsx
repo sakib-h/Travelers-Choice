691cb0696c56e0679b2e311f18623759ce2897c5
Sakib Hasan <sakib100.sa@gmail.com>
2023-01-03 05:52:33 +0600

    login page css added on client side

--- client/src/Pages/Auth/Login.jsx
+++ client/src/Pages/Auth/Login.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "../../Components/Header/Header";
+import Navbar from "../../Components/Navbar/Navbar";
 import { AuthContext } from "../../Context/AuthContext";
 
 const Login = () => {
@@ -31,26 +33,38 @@ const Login = () => {
 	};
 
 	return (
-		<div className="container">
-			<input
-				type="text"
-				name="username"
-				id="username"
-				placeholder="Username"
-				onChange={handleChange}
-			/>
-			<input
-				type="password"
-				name="password"
-				id="password"
-				placeholder="Password"
-				onChange={handleChange}
-			/>
-			<button className="" disabled={loading} onClick={handleClick}>
-				Login
-			</button>
-			{error && <span>{error.message}</span>}
-		</div>
+		<section>
+			<Navbar />
+			<Header />
+			<div className="container">
+				<div className="title text-[18px] mt-5">User Login</div>
+				<div className="max-w-[350px] flex flex-col">
+					<input
+						type="text"
+						name="username"
+						id="username"
+						placeholder="Username"
+						onChange={handleChange}
+						className="w-full my-10 border-b-[1px] border-[lightgray]"
+					/>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="Password"
+						onChange={handleChange}
+						className="w-full mb-10 border-b-[1px] border-[lightgray]"
+					/>
+					<button
+						className="w-max bg-[#003580] text-white font-[700] py-[10px] px-[20px] rounded-[5px]"
+						disabled={loading}
+						onClick={handleClick}>
+						Login
+					</button>
+					{error && <span>{error.message}</span>}
+				</div>
+			</div>
+		</section>
 	);
 };
 
